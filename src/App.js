4b6e488c61e522af47fb26a855bb17a6a636a757
Alhaj7 <95449961+Alhaj7@users.noreply.github.com>
2023-03-29 14:24:07 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Forum from './pages/Forum'
+import HolidayRequest from './pages/HolidayRequest'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Hello!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Sidebar>
+   <Routes>
+   <Route path="/" element={<Dashboard />} />
+    <Route path="/dashboard"element={<Dashboard/>}/>
+    <Route path="/about"element={<About/>}/>
+    <Route path="/forum"element={<Forum/>}/>
+    <Route path="/holidayrequest"element={<HolidayRequest/>}/>
+   </Routes>
+   </Sidebar>
+   </BrowserRouter>
   );
-}
+};
 
 export default App;
